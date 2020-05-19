f740e5435ec65095ced3123e0e7a58ca2a9f09d2
Oscar Nunez <noscar07@gmail.com>
2020-05-19 18:29:14 -0400

    push

---
+++ reactbio/components/index/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const IndexPage=()=>{
+    const context=React.useContext(GlobalContext);
+return <div>Hello {context.globalState.currentAccount.username}, you have <b>{context.globalState.tasksList.filter(task=>task.username===context.globalState.currentAccount&&!task.completed).length}</b> number of tasks pending.
+<TasksList status="pending" />
+<div style={{marginTop:"40px",textAlign:"center"}}>
+<Link to="/completed"> View My Completed Tasks</Link>
+    <Link to="/task/create" className="btn btn-primary"> Add a new Task</Link>
+</div>
+</div>
+}
+export default IndexPage;
